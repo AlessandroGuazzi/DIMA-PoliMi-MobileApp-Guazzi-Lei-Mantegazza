5d28321c53d0b7255cf753a21d0dfa062c0fbea2
NMantegazza <niccolo.mantegazza@mail.polimi.it>
2025-02-27 13:22:48 +0100

    folder organization

---
+++ lib/screens/authenticationPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthenticationPage extends StatelessWidget {
+  const AuthenticationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
