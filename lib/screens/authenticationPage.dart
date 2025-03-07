7f92654ec806c45f557e28412a3a9309015de9a5
Alessandro <alessandro.guazzi@mail.polimi.it>
2025-03-07 19:11:44 +0100

    baseline for authentication

--- lib/screens/authenticationPage.dart
+++ lib/screens/authenticationPage.dart
@@ -1,10 +1,59 @@
+import 'package:dima_project/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class AuthenticationPage extends StatelessWidget {
-  const AuthenticationPage({super.key});
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool isLogin = true;
+
+  Future<void> signIn() async{
+    try{
+      await Auth().signInWithEmailAndPassword(email: _email.text, password: _password.text);
+    }on FirebaseAuthException catch (error){}
+  }
+
+  Future<void> createUser() async{
+    try{
+      await Auth().createUserWithEmailAndPassword(email: _email.text, password: _password.text);
+    }on FirebaseAuthException catch (error){}
+  }
 
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Demo App'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: InputDecoration(label: Text('email')),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            decoration: InputDecoration(label: Text('password')),
+          ),
+          ElevatedButton(onPressed: (){
+            isLogin ? signIn() : createUser();
+          }, child: Text(isLogin ? 'Accedi' : 'Registrati')),
+          TextButton(onPressed: (){
+            setState(() {
+              isLogin = !isLogin;
+            });
+          }, child: Text(isLogin ? 'Non hai un account? Registrati' : 'Hai un account? Accedi'))
+        ]
+      ),
+    );
   }
 }
