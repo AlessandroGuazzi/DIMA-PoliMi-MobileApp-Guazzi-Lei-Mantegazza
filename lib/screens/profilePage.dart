bcc8f4e3a3ea191a32cbdbd60fcd4ff49f5e6459
Alessandro <alessandro.guazzi@mail.polimi.it>
2025-03-19 11:55:50 +0100

    Profile Page update + Icon

--- lib/screens/profilePage.dart
+++ lib/screens/profilePage.dart
@@ -1,3 +1,4 @@
+import 'package:dima_project/utils/screenSize.dart';
 import 'package:flutter/material.dart';
 import 'package:dima_project/services/authService.dart';
 
@@ -14,250 +15,79 @@ class _ProfilePageState extends State<ProfilePage> {
     await AuthService().signOut();
   }
 
-  /* Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        top: true,
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          child: Align(
-            alignment: const AlignmentDirectional(1, 0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: 200,
-                        height: 200,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: Image.network(
-                          'https://picsum.photos/seed/65/600',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                
-                Container(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Ciao User, Bentornato!',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          fontSize: 30
-                        ),
-                      ),
-                      ListView(
-                        padding: EdgeInsets.zero,
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          ListTile(
-                            title: Text(
-                              'Title',
-                            )
-                          ),
-                          TextButton.icon(
-                              onPressed: (){},
-                              icon: const Icon(Icons.person_2_outlined),
-                              label: Text('Personal Details')
-                          ),
-                          TextButton.icon(
-                              onPressed: (){},
-                              icon: const Icon(Icons.add_business_rounded),
-                              label: Text('Saved')
-                          ),
-                          TextButton.icon(
-                              onPressed: (){},
-                              icon: const Icon(Icons.settings_outlined),
-                              label: Text('Settings')
-                          ),
-                          TextButton.icon(
-                              onPressed: (){},
-                              icon: const Icon(Icons.logout_outlined),
-                              label: Text('Log Out')
-                          )
-                        ],
-                      ),
-                    ]
-                  )
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }*/
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        top: true,
-        child: Container(
-          padding: const EdgeInsets.all(40),
-          child: Align(
-            alignment: const AlignmentDirectional(1, 0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                // Immagine Profilo
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: 200,
-                        height: 200,
-                        clipBehavior: Clip.antiAlias,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: Image.network(
-                          'https://picsum.photos/seed/65/600',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                // Informazioni Utente
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      // Saluto Utente
-                      const Text(
-                        'Ciao User, Bentornato!',
-                        style: TextStyle(
-                            fontSize: 30
-                        ),
-                      ),
-
-                      // Spazio tra saluto e lista
-                      const SizedBox(height: 20),
-
-                      // Lista Opzioni
-                      ListView(
-                        padding: EdgeInsets.zero,
-                        shrinkWrap: true,
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          // Opzioni Utente
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor, // Sfondo blu
-                              shape: RoundedRectangleBorder( // Bordi arrotondati
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              padding: const EdgeInsets.all(20), // Spaziatura interna
-                            ),
-                            child: const Row(
-                              children: [
-                                Icon(Icons.person_2_outlined, color: Colors.white), // Icona bianca
-                                SizedBox(width: 16), // Spazio tra icona e testo
-                                Expanded(
-                                  child: Text(
-                                    'Personal Details',
-                                    style: TextStyle(color: Colors.white), // Testo bianco
-                                  ),
-                                ),
-                                Icon(Icons.arrow_forward_ios_rounded, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 10), // Spazio tra bottoni
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor, // Sfondo blu
-                              shape: RoundedRectangleBorder( // Bordi arrotondati
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              padding: const EdgeInsets.all(20), // Spaziatura interna
-                            ),
-                            child: const Row(
-                              children: [
-                                Icon(Icons.favorite, color: Colors.white), // Icona bianca
-                                SizedBox(width: 16), // Spazio tra icona e testo
-                                Expanded(
-                                  child: Text(
-                                    'Saved',
-                                    style: TextStyle(color: Colors.white), // Testo bianco
-                                  ),
-                                ),
-                                Icon(Icons.arrow_forward_ios_rounded, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 10), // Spazio tra bottoni
-                          TextButton(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor, // Sfondo blu
-                              shape: RoundedRectangleBorder( // Bordi arrotondati
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              padding: const EdgeInsets.all(20), // Spaziatura interna
-                            ),
-                            child: const Row(
-                              children: [
-                                Icon(Icons.settings_outlined, color: Colors.white), // Icona bianca
-                                SizedBox(width: 16), // Spazio tra icona e testo
-                                Expanded(
-                                  child: Text(
-                                    'Settings',
-                                    style: TextStyle(color: Colors.white), // Testo bianco
-                                  ),
-                                ),
-                                Icon(Icons.arrow_forward_ios_rounded, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                          const SizedBox(height: 10), // Spazio tra bottoni
-                          TextButton(
-                            onPressed: () {
-                              signOut();
-                            },
-                            style: TextButton.styleFrom(
-                              backgroundColor: Theme.of(context).primaryColor, // Sfondo blu
-                              shape: RoundedRectangleBorder( // Bordi arrotondati
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              padding: const EdgeInsets.all(20), // Spaziatura interna
-                            ),
-                            child: const Row(
-                              children: [
-                                Icon(Icons.logout_outlined, color: Colors.white), // Icona bianca
-                                SizedBox(width: 16), // Spazio tra icona e testo
-                                Expanded(
-                                  child: Text(
-                                    'Log Out',
-                                    style: TextStyle(color: Colors.white), // Testo bianco
-                                  ),
-                                ),
-                                Icon(Icons.arrow_forward_ios_rounded, color: Colors.white)
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0xFFF1F4F8),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            'Profilo',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings, color: Colors.black),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              Container(
+                width: ScreenSize.screenWidth(context)*0.30,
+                height: ScreenSize.screenHeight(context)*0.15,
+                //width: 150,
+                //height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Theme.of(context).primaryColor,
+                    width: 2,
+                  ),
+                  image: const DecorationImage(
+                    image: AssetImage('assets/profile.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Marco Rossi',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 24),
+              TabBar(
+                indicatorColor: Theme.of(context).primaryColor,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: const [
+                  Tab(text: 'Viaggi Salvati'),
+                  Tab(text: 'I Tuoi Viaggi'),
+                ],
+              ),
+              const Expanded(
+                child: TabBarView(
+                  children: [
+                    Center(child: Text('Lista dei Viaggi Salvati')),
+                    Center(child: Text('Lista dei Tuoi Viaggi')),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
